906751633e1fdce4211782ba3511a71e8b15de52
tungtomhum <tung01662025039@gmail.com>
2023-09-18 16:40:02 +0700

    SachOnline

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
