dbf07ff24223579c84da9a12632a2c33454ae56e
tungtomhum <tung01662025039@gmail.com>
2023-09-19 13:36:01 +0700

    fix

--- SachOnline/App_Start/RouteConfig.cs
+++ SachOnline/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SachOnline
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SachOnline", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
